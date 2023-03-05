03904674f3021efbc17d7a34e2de3b3a23b8a8f6
LingMoYuXi <781597853@qq.com>
2023-03-06 00:07:33 +0800

    头像上传方法完成。上传文件路径调整至同项目路径下，接口为static。

--- src/main/java/com/example/lifediary/config/WebConfig.java
+++ src/main/java/com/example/lifediary/config/WebConfig.java
@@ -1,9 +1,14 @@
 package com.example.lifediary.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.io.File;
 
 /**
  * 描述:WebConfig
@@ -12,13 +17,29 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
  * @author MoYuXi
  */
 @Configuration
+@Slf4j
 public class WebConfig extends WebMvcConfigurationSupport {
     @Override
-    protected void configurePathMatch(PathMatchConfigurer configurer) {
+    protected void configurePathMatch(@NotNull PathMatchConfigurer configurer) {
         super.configurePathMatch(configurer);
         //url忽略大小写
         AntPathMatcher matcher = new AntPathMatcher();
         matcher.setCaseSensitive(false);
         configurer.setPathMatcher(matcher);
+    }
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //资源文件路径配置
+        String property = System.getProperty("user.dir");
+        File file = new File(property, "static/");
+        String path = "file:" + file + File.separator;
+        log.info("File upload to Path ------>" + path);
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:META-INF/resources/",
+                        "classpath:/resources/",
+                        "classpath:/static/",
+                        "classpath:/public/",
+                        path);
     }
 }
--- src/main/java/com/example/lifediary/controller/FileController.java
+++ src/main/java/com/example/lifediary/controller/FileController.java
@@ -1,8 +1,10 @@
 package com.example.lifediary.controller;
 
 import com.example.lifediary.dto.Result;
+import com.example.lifediary.entity.User;
+import com.example.lifediary.utils.JwtUtils;
 import io.swagger.annotations.Api;
-import org.springframework.util.ResourceUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +13,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.UUID;
 
 /**
  * 描述:文件控制器
@@ -21,20 +25,59 @@ import java.io.FileNotFoundException;
 @Api(tags = "文件模块")
 @RestController
 @RequestMapping("/file")
+@Slf4j
 public class FileController extends BaseController {
     @PostMapping("/upload")
-    public Result upLoadFile(@RequestParam("file") MultipartFile file) throws FileNotFoundException {
-//        return Result.success(file.getOriginalFilename());
-        return Result.success(ResourceUtils.getURL("classpath:").getPath());
+    public Result upLoadFile(@RequestParam("file") MultipartFile file) {
+        try {
+            return Result.success(getPath("upload\\icon") + file.getOriginalFilename());
+        } catch (FileNotFoundException e) {
+            return Result.error(e.getMessage());
+        }
     }
 
-    public String getPath() throws FileNotFoundException {
-        File path = new File(ResourceUtils.getURL("classpath:").getPath());
-        if (!path.exists()) {
-            path = new File("");
-            return path.getAbsolutePath();
+    @PostMapping("/uploadIcon")
+    public Result upLoadIcon(@RequestParam("file") MultipartFile file) {
+        Integer userId = JwtUtils.getUserId(request);
+        if (userId == -1) {
+            return Result.error(401, "Token验证未通过或已失效，请重新登录获取token！");
         }
-        return null;
+        String path = "upload/icon/";
+        String originalFilename = file.getOriginalFilename();
+        String suffixName;
+        if (originalFilename != null) {
+            suffixName = originalFilename.substring(originalFilename.lastIndexOf("."));
+        } else return Result.error("空文件名");
+        String filename = UUID.randomUUID() + suffixName;
+        try {
+            User user = userService.getById(userId);
+            String icon = user.getIcon();
+            File iconFile = new File(getPath(path), filename);
+            file.transferTo(iconFile);
+            user.setIcon("static/" + path + filename);
+            if (icon != null && !icon.isEmpty()) {
+                File oldIconFile = new File(getPath(icon));
+                if (oldIconFile.isFile() && oldIconFile.exists()) oldIconFile.delete();
+            }
+            userService.updateById(user);
+            return Result.success(user.getIcon());
+        } catch (IOException e) {
+            return Result.error("文件处理异常：" + e.getMessage());
+        }
+    }
+
+    /**
+     * 文件保存目录至项目同目录下
+     *
+     * @param subdirectory 添加至目录路径
+     * @return 完整的文件路径
+     * @throws FileNotFoundException
+     */
+    public String getPath(String subdirectory) throws FileNotFoundException {
+        String property = System.getProperty("user.dir");
+        File file = new File(property, subdirectory.startsWith("static/") ? subdirectory : "static/" + subdirectory);
+        if (!file.exists()) file.mkdirs();
+        return file.getPath();
     }
 
 }
